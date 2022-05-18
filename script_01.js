2caf51aa903fd3ebd02a07a256e3bbae81ab757b
Zubair Yousaf-Eberl <103026463+Zubair-Eb@users.noreply.github.com>
2022-05-18 14:53:47 +0200

    Objects / Hierarchie

--- script_01.js
+++ script_01.js
@@ -10,12 +10,42 @@
 
 
 /***** Objekte 1 Daten/Funktionen *******/
+let person = {
+        firstName: "Rocky",
+        familyName: "Eberl",
+        salary: [120000,160000],
+        permission: true,
+        sayHello: 
+        function () {
+                return "Hello, " + this.firstName;
+        }
+
+};
 
 
+
+// output(person.sayHello());
+// output(person.firstName);
+// output(person.permission);
+
+// const txt = "Ich bin " + person.firstName + " " +
+//         person.familyName + " und verdiene "+ person.salary[0] +" Euro per p.a"+".";
+//         output(txt);
 
 
 /***** Objekte 2 - Hierarchie *******/
+const baikal = {
+        value: "10m",
+        deep: {
+                        deeper: {
+                                deepest: "Das Licht - auf 1642m!"
+                        }
+        }
 
+};
+        
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
 
 
 
